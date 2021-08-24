bde7690202f2c0c5acac3086f08a1926225c41ff
Ricardo Malnati <ricardo.malnati@tarea.com.br>
2021-08-24 19:15:04 -0300

    feat(red): score service unit tests

---
+++ src/test/java/be/bnpparibasfortis/test/unit/service/ScoreServiceTest.java
@@ -0,0 +1,57 @@
+package be.bnpparibasfortis.test.unit.service;
+
+import java.util.Optional;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.Mockito;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.core.annotation.Order;
+
+import be.bnpparibasfortis.exception.BusinessException;
+import be.bnpparibasfortis.model.Score;
+import be.bnpparibasfortis.repository.ScoreRepository;
+import be.bnpparibasfortis.service.ScoreService;
+
+@SpringBootTest
+@DisplayName("A unit test for ScoreService")
+public class ScoreServiceTest {
+
+	private static final Integer DEFAULT_POINTS = 0;
+	private static final Integer INCREASE = 1;
+	private static final Long SCORE_ID = 1L;
+
+	@Mock
+	private ScoreRepository scoreRepository;
+
+	@InjectMocks
+	private ScoreService scoreService;
+
+	@Test
+	@Order(1)
+	@DisplayName("Tests the retrieving of a score.")
+	public void getById() throws BusinessException {
+		Assertions.assertDoesNotThrow(() -> {
+			Score score = scoreService.getById(SCORE_ID);
+			Assertions.assertAll("score check",
+					() -> Assertions.assertTrue(score.equals(new Score(SCORE_ID, DEFAULT_POINTS))));
+		});
+	}
+
+	@Test
+	@Order(2)
+	@DisplayName("Tests the update of a score.")
+	public void update() throws BusinessException {
+		Assertions.assertDoesNotThrow(() -> {
+			Integer newPoints = DEFAULT_POINTS + INCREASE;
+
+			Score score = scoreService.update(SCORE_ID, newPoints);
+
+			Assertions.assertAll("updated score check",
+					() -> Assertions.assertTrue(score.equals(new Score(SCORE_ID, newPoints))));
+		});
+	}
+}
